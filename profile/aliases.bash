4ac3b8732e5a5040c2edc2826cc66d950a19cd9e
Marcelo Manzan <manzan@gmail.com>
2011-05-26 20:42:51 -0300

    improvements on make_alias

--- profile/aliases.bash
+++ profile/aliases.bash
@@ -1,7 +1,32 @@
 #!/usr/bin/env bash
 
 echo_run(){ echo -e "\033[0;36m$*\033[0m" && $*; }
-make_alias(){ alias $1="_(){ echo -e \"\033[0;36m$2\033[0m\"; $2; } && _"; }
+make_alias(){
+  name="$1"
+  shift
+  sentence="("
+  while [ "$1" != "" ]
+  do
+    if echo $1 | egrep -q '^[0-9]+$'; then
+      if [ "$sentence" != "(" ]; then
+        sentence="$sentence) || ("
+      fi
+      sentence="$sentence [[ \"\$#\" = \"$1\" ]]"
+    else
+      if [ "$sentence" != "(" ]; then
+        sentence="$sentence &&"
+      fi
+      sentence="$sentence echo -e \"\033[0;36m$1\033[0m\" && $1"
+    fi
+    shift
+  done
+  if [ "$sentence" != "(" ]; then
+    sentence="$sentence )"
+  else
+    sentence=""
+  fi
+  alias $name="_(){ $sentence; } && _";
+}
 alias ll='ls -lhFG'
 
 # rake
@@ -13,7 +38,7 @@ alias gh="echo_run git push"
 alias glrh="gl && r && gh"
 
 # ps
-make_alias psg "ps -aef | grep \$*"
+make_alias psg 1 "ps -aef | grep --color \$@"
 
 if [ -e ~/profile/private_aliases.bash ]; then
   . ~/profile/private_aliases.bash
